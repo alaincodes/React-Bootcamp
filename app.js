c53876ff80cc1508c4bc264f0e71741c2fd65aa1
Alain Tran <alaint77@gmail.com>
2019-06-10 20:17:21 +0200

    Learning State and Lifecycle

--- app.js
+++ app.js
@@ -1,142 +1,61 @@
-// const name = "Alain";
-// const handle = "@alaincodes";
+// State and Lifecycle
 
-// function NameComponent(props) {
-//   return <h1>{props.name}</h1>;
+// function tick() {
+//   const element = (
+//     <div>
+//       <h1>Hello, World!</h1>
+//       <h2>It is {new Date().toLocaleTimeString()}.</h2>
+//     </div>
+//   );
+//   ReactDOM.render(element, document.getElementById("app"));
 // }
 
-// function HandleComponent(props) {
-//   return <h3>{props.handle}</h3>;
-// }
+// setInterval(tick, 1000);
 
-// function App() {
+// function Clock(props) {
 //   return (
-//     <div id="container">
-//       <NameComponent name={name} />
-//       <HandleComponent handle={handle} />
+//     <div>
+//       <h1>Hello, World!</h1>
+//       <h2>It is {props.date.toLocaleTimeString()}.</h2>
 //     </div>
 //   );
 // }
 
-// ReactDOM.render(<App />, document.getElementById("app"));
+// function tick() {
+//   ReactDOM.render(<Clock />, document.getElementById("app"));
+// }
 
-window.API = {
-  fetchPopularRepos(language = "all") {
-    const encodedURI = encodeURI(
-      `https://api.github.com/search/repositories?q=stars:>1+language:${language}&sort=stars&order=desc&type=Repositories`
-    );
-    return fetch(encodedURI)
-      .then(data => data.json())
-      .then(repos => repos.items)
-      .catch(error => {
-        console.warn(error);
-        return null;
-      });
-  }
-};
+// setInterval(tick, 1000);
 
-class Loading extends React.Component {
+class Clock extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-      text: "Loading"
-    };
+    this.state = { date: new Date() };
   }
+
+  // Lifecyle methods componentDidMount and componentWillUnmount
   componentDidMount() {
-    const stopper = this.state.text + "...";
-    this.interval = window.setInterval(() => {
-      this.state.text === stopper
-        ? this.setState(() => ({ text: "Loading" }))
-        : this.setState(prevState => ({ text: prevState.text + "." }));
-    }, 300);
+    this.timerID = setInterval(() => this.tick(), 1000);
   }
+
   componentWillUnmount() {
-    window.clearInterval(this.interval);
+    clearInterval(this.timerID);
   }
-  render() {
-    return <p>{this.state.text}</p>;
-  }
-}
-function RepoGrid(props) {
-  return (
-    <ul style={{ display: "flex", flexWrap: "wrap" }}>
-      {props.repos.map(({ name, owner, stargazers_count, html_url }) => (
-        <li key={name} style={{ margin: 30 }}>
-          <ul>
-            <li>
-              <a href={html_url}>{name}</a>
-            </li>
-            <li>@{owner.login}</li>
-            <li>{stargazers_count} stars</li>
-          </ul>
-        </li>
-      ))}
-    </ul>
-  );
-}
-function Nav(props) {
-  const languages = ["all", "javascript", "ruby", "python"];
-  return (
-    <nav>
-      <ul>
-        {languages.map(lang => (
-          <li key={lang} onClick={() => props.onSelectLanguage(lang)}>
-            {lang}
-          </li>
-        ))}
-      </ul>
-    </nav>
-  );
-}
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      repos: [],
-      activeLanguage: "all",
-      loading: true
-    };
-    this.handleSelectLanguage = this.handleSelectLanguage.bind(this);
-    this.fetchRepos = this.fetchRepos.bind(this);
-  }
-  componentDidMount() {
-    this.fetchRepos(this.state.activeLanguage);
-  }
-  componentDidUpdate(prevProps, prevState) {
-    if (prevState.activeLanguage !== this.state.activeLanguage) {
-      this.fetchRepos(this.state.activeLanguage);
-    }
-  }
-  fetchRepos(lang) {
+
+  tick() {
     this.setState({
-      loading: true
-    });
-    window.API.fetchPopularRepos(lang).then(data => {
-      this.setState({
-        loading: false,
-        repos: data
-      });
+      date: new Date()
     });
   }
-  handleSelectLanguage(lang) {
-    this.setState({
-      activeLanguage: lang
-    });
-  }
+
   render() {
     return (
       <div>
-        <Nav onSelectLanguage={this.handleSelectLanguage} />
-        {this.state.loading === true ? (
-          <Loading />
-        ) : (
-          <div>
-            <h1 style={{ textAlign: "center" }}>{this.state.activeLanguage}</h1>
-            <RepoGrid repos={this.state.repos} />
-          </div>
-        )}
+        <h1>Hello, World</h1>
+        <h2>It is {this.state.date.toLocaleTimeString()}</h2>
       </div>
     );
   }
 }
-ReactDOM.render(<App />, document.getElementById("app"));
+
+ReactDOM.render(<Clock />, document.getElementById("app"));
